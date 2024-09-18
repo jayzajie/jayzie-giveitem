0a042db814c22c23563dc69f03533ebe01935120
Muhammad Surya Wijaya <150271594+jayzajie@users.noreply.github.com>
2024-09-19 07:08:49 +0800

    new update, only admin can access

--- server.lua
+++ server.lua
@@ -1,22 +1,29 @@
 ESX = exports["es_extended"]:getSharedObject()
 
 ESX.RegisterCommand('gvitem', {'admin'}, function(xPlayer, args, showError)
-    TriggerClientEvent('gvitem:showGiveItemDialog', xPlayer.source)
+    if xPlayer.getGroup() == 'admin' then
+        TriggerClientEvent('gvitem:showGiveItemDialog', xPlayer.source)
+    else
+        TriggerClientEvent('ox_lib:notify', xPlayer.source, {type = 'error', description = 'You do not have permission to use this command.'})
+    end
 end, true, {help = 'Give item to player', validate = true})
 
 RegisterNetEvent('gvitem:giveItemToPlayer', function(targetId, itemName, itemQuantity)
-    local xTarget = ESX.GetPlayerFromId(targetId)
     local xSource = ESX.GetPlayerFromId(source)
+    if xSource.getGroup() == 'admin' then
+        local xTarget = ESX.GetPlayerFromId(targetId)
 
-    if xTarget then
-        if exports.ox_inventory:CanCarryItem(targetId, itemName, itemQuantity) then
-            exports.ox_inventory:AddItem(targetId, itemName, itemQuantity)
-
-            TriggerClientEvent('ox_lib:notify', source, {type = 'success', description = 'Item successfully given!'})
+        if xTarget then
+            if exports.ox_inventory:CanCarryItem(targetId, itemName, itemQuantity) then
+                exports.ox_inventory:AddItem(targetId, itemName, itemQuantity)
+                TriggerClientEvent('ox_lib:notify', source, {type = 'success', description = 'Item successfully given!'})
+            else
+                TriggerClientEvent('ox_lib:notify', source, {type = 'error', description = 'Target player cannot carry this item.'})
+            end
         else
-            TriggerClientEvent('ox_lib:notify', source, {type = 'error', description = 'Target player cannot carry this item.'})
+            TriggerClientEvent('ox_lib:notify', source, {type = 'error', description = 'Player not found.'})
         end
     else
-        TriggerClientEvent('ox_lib:notify', source, {type = 'error', description = 'Player not found.'})
+        TriggerClientEvent('ox_lib:notify', source, {type = 'error', description = 'You are not authorized to execute this action.'})
     end
 end)
